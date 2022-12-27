66873f6f857bf40bfaa6e2cf432c04b9eaf95ca5
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2022-12-27 14:43:17 +0700

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import { Component } from "react";
+import "./Navbar.Styles.css";
+import { MenuItems } from "./MenuItems";
+
+class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">TripAPP</h1>
+
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i className={this.state.clicked ? "fas fa-times" : "fa fa-bars"}></i>
+        </div>
+
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href="/">
+                  <i className={item.icon}></i>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
